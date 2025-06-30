0fc9b551d820a837e56b3080135adfbfc6600ee2
Earth Walker <pineconefirestarter@pm.me>
2025-06-30 14:31:06 -0400

    configure site for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://earth-walker.github.io",
+  base: "/bdt-docs",
   integrations: [
     starlight({
       title: "My Docs",
